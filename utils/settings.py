aa68595b92d06cac9c22dbc0f9c86e92431468bd
yuq <zxdewr@sjtu.edu.cn>
2017-04-12 15:16:53 +0800

    Modify: Move settings to utils directory.
    I'll rename utils to a better name in a few commits.

--- utils/settings.py
+++ utils/settings.py
@@ -14,10 +14,12 @@ NORMAL_SUBMIT_URL = ELECT_URL+'/electSubmit.aspx'
 RECOMMAND_URL = ELECT_URL+'/RecommandTblOuter.aspx'
 SUMMER_URL = ELECT_URL + '/ShortSession.aspx'
 SUMMER_CHECK_URL_TEMPLATE = ELECT_URL+'/ShortSessionCheck.aspx?xklc=%d'
-SELECT_COURSE_URL = LESSON_URL+'?&xklx=&redirectForm=outSpeltyEp.aspx&kcmk=-1'
+SELECT_NORMAL_COURSE_URL = LESSON_URL+'?&xklx=&redirectForm=outSpeltyEp.aspx&kcmk=-1'
+SELECT_SUMMER_COURSE_URL = LESSON_URL+'?&xklx=&redirectForm=ShortSession.aspx&kcmk=-1'
 SUMMER_SUBMIT_URL = ELECT_URL+'/electSubmitShort.aspx?redirectForm=ShortSession.aspx'
 JACCOUNT_URL = 'https://jaccount.sjtu.edu.cn/jaccount/'
 
 TONGSHI_NAMES = ['人文学科', '社会科学', '数学或逻辑', '自然科学与']
 
 CACHE_SESSION_PATH = '/tmp/session.pickle'
+COURSE_DATA_PATH = 'data/course.json'
